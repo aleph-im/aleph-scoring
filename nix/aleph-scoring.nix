e2c5459960c74dffc5be73b118eed05632d463c9
Hugo Herter <git@hugoherter.com>
2024-09-20 17:51:50 +0200

    Update deployed revision

--- nix/aleph-scoring.nix
+++ nix/aleph-scoring.nix
@@ -3,7 +3,7 @@
   alephScoring = pkgs.callPackage (pkgs.fetchFromGitHub {
     owner = "aleph-im";
     repo = "aleph-scoring";
-    rev = "b0b652465b65db0947fdbeac72ee8cf86f717937";
-    sha256 = "sha256-+rYKzzxQTrJ3AO2nKK6Y4+gHKvLNB8LzBABa2h4F+UA=";
+    rev = "b07ac8bc2bfaddd600f949fd5125c64d8e457318";
+    sha256 = "sha256-oSfGZhwAwPC6yzLGysgjyTNaCMH5AC0lcObrPTTcqYw=";
   }) { };
 }
