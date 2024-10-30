661c18a5ff2d08fb40712f87a156288f87478118
Hugo Herter <git@hugoherter.com>
2024-10-30 18:00:58 +0100

    Candidate function for the new score
    Renamed SQL functiont to avoid name collisions

--- aleph_scoring/scoring/sql/dev/neo/06.sql
+++ aleph_scoring/scoring/sql/dev/neo/06.sql
@@ -7,6 +7,7 @@ WITH base_query AS (
         percentile_cont(0.67) WITHIN GROUP (ORDER BY (node -> 'base_latency')::float) AS base_latency_67th_percentile,
         percentile_cont(0.67) WITHIN GROUP (ORDER BY (node -> 'diagnostic_vm_latency')::float) AS diagnostic_vm_latency_67th_percentile,
         percentile_cont(0.67) WITHIN GROUP (ORDER BY (node -> 'full_check_latency')::float) AS full_check_latency_67th_percentile,
+        MAX(is_version_valid('aleph-vm', node ->> 'version', to_timestamp((node ->> 'measured_at')::float)::date)) AS version_valid,
 
         EXTRACT(EPOCH FROM AGE(%(to_date)s::timestamp, date_trunc('hour', posts.creation_datetime))) / 3600 AS hours_difference
     FROM posts,
@@ -20,17 +21,26 @@ WITH base_query AS (
 )
 SELECT
     node_id,
+    SUM(version_valid) as valid_versions,
     SUM(
         (
             geometric_pmf(%(p1)s, CEIL(hours_difference)) * %(p1_ratio)s
             +
             geometric_pmf(%(p2)s, CEIL(hours_difference)) * %(p2_ratio)s
         ) * (
-            GREATEST(1 - ((base_latency_67th_percentile ^ 2) / 4), 0) *
-            GREATEST(1 - ((diagnostic_vm_latency_67th_percentile ^ 2) / 4), 0) *
-            GREATEST(1 - ((full_check_latency_67th_percentile ^ 2) / 40), 0)
-        ) ^ (1/3.0)
-    ) AS total_score
+            (
+                GREATEST(1 - ((base_latency_67th_percentile ^ 2) / 4), 0) *
+                GREATEST(1 - ((diagnostic_vm_latency_67th_percentile ^ 2) / 4), 0) *
+                GREATEST(1 - ((full_check_latency_67th_percentile ^ 2) / 40), 0)
+            ) ^ (1/3.0)
+        ) * (
+            version_valid
+        ) * (
+            -- This multiplier moves the score of the large majority of nodes above 80%.
+            1.1
+        )
+    ) AS total_score,
+    COUNT(*) as record_count
 FROM base_query
 GROUP BY node_id
 ORDER BY total_score DESC;
